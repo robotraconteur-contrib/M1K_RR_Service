c842ac6bb9adf4dc349b86e6999bcffff7796467
hehonglu123 <hehonglu123@126.com>
2020-09-04 16:36:27 -0400

    mmm

--- client.m
+++ client.m
@@ -4,46 +4,48 @@ m1k_obj=RobotRaconteur.ConnectService('rr+tcp://localhost:11111?service=m1k');
 m1k_obj.setmode('A','SVMI');
 m1k_obj.setmode('B','HI_Z');
 periodvalue=100;
-m1k_obj.wave('A', 'sine', 0, 5, periodvalue, -(periodvalue / 4), 0.5);
+% m1k_obj.wave('A', 'sine', 0, 5, periodvalue, -(periodvalue / 4), 0.5);
 
-% %read 2000 samples and plotting
-% samples=m1k_obj.read(int16(2000));
-% y=zeros(length(samples));
-% for i=1:length(samples)
-% 	y(i)=samples{i}.A(1);
+
+t = 0:1/1e3:1;
+y = 5.*double(chirp(t,0,1,50,'quadratic'));
+m1k_obj.arbitrary('A',y');
+
+
+y_samples=m1k_obj.read(int16(1000));
+plot(y_samples(1:4:end))
+
+
+
+% % streaming
+% try
+%     m1k_obj.StartStreaming();
+% catch 
+% end
+% y=zeros(periodvalue);
+% timestamp_seconds=0;
+% timestamp_nano=0;
+% % connnect to wire
+% samples_wire=m1k_obj.samples.Connect();
+% % plot
+% h=plot(y,'YDataSource','y');
+% while true
+%     timestamp=samples_wire.LastValueReceivedTime;    
+%     if timestamp_seconds==timestamp.seconds && timestamp_nano== timestamp.nanoseconds
+%             continue
+%     else
+%         timestamp_seconds=timestamp.seconds;
+%         timestamp_nano= timestamp.nanoseconds;
+%         y=circshift(y,1);
+%         sample=samples_wire.InValue;
+%         y(1)=sample(1);
+%         refreshdata(h,'caller') 
+%         drawnow
+%     end
 % end
 % 
-% plot(y)
-
-% streaming
-try
-    m1k_obj.StartStreaming();
-catch 
-end
-y=zeros(periodvalue);
-timestamp_seconds=0;
-timestamp_nano=0;
-% connnect to wire
-samples_wire=m1k_obj.samples.Connect();
-% plot
-h=plot(y,'YDataSource','y');
-while true
-    timestamp=samples_wire.LastValueReceivedTime;    
-    if timestamp_seconds==timestamp.seconds && timestamp_nano== timestamp.nanoseconds
-            continue
-    else
-        timestamp_seconds=timestamp.seconds;
-        timestamp_nano= timestamp.nanoseconds;
-        y=circshift(y,1);
-        sample=samples_wire.InValue;
-        y(1)=sample.A(1);
-        refreshdata(h,'caller') 
-        drawnow
-    end
-end
-
-% stop streaming
-m1k_obj.StopStreaming();
+% % stop streaming
+% m1k_obj.StopStreaming();
 
 
 
